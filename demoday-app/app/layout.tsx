d1b3191594278ea18f5701c17cc0a0715a258a03
binaryshrey <shreyansh.saurabh01@gmail.com>
2025-12-31 16:33:56 -0500

    adds vercel analytics

--- demoday-app/app/layout.tsx
+++ demoday-app/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthKitProvider } from "@workos-inc/authkit-nextjs/components";
 import { withAuth } from "@workos-inc/authkit-nextjs";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
