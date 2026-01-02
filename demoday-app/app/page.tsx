5892e559f046294109a2ab33b37fba5acd5994a0
binaryshrey <shreyansh.saurabh01@gmail.com>
2026-01-02 18:03:29 -0500

    adds features page

--- demoday-app/app/page.tsx
+++ demoday-app/app/page.tsx
@@ -1,3 +1,4 @@
+import Features from "@/components/view/feat/Features";
 import HeroSection from "../components/view/HeroSection";
 import {
   withAuth,
@@ -13,6 +14,7 @@ export default async function Home() {
   return (
     <div>
       <HeroSection user={user} signInUrl={signInUrl} signUpUrl={signUpUrl} />
+      <Features />
     </div>
   );
 }
